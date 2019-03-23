5c3ebfe725a0307232cb2e98ac6e5eba46708443
Andrew <acoley551@gmail.com>
2019-03-23 15:38:58 -0700

    New push

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-function setup() {
+var backgrounColor;
+var x = 50;
+var y = 50;
 
+function setup() {
+  createCanvas(windowWidth, windowHeight); 
+  backgroundColor = color(255, 0, 220);
+  
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(251, 255, 232);
+  }
+  background(backgroundColor);
+  ellipse(x, y, 25, 25);
+  x = x + 1;
+  y = y + 1;
 }
---
+++ second.js
@@ -0,0 +1,7 @@
+function setup() {
+	craeteCanvas(400, 400)
+}
+
+function draw() {
+	backgound(220, 0, 200);
+}
